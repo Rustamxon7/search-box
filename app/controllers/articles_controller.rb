ffcd0949a42749f24714ffab276aeb32b23212b6
rustamtolipov <rustamxontolipov788@gmail.com>
2023-04-28 09:03:37 +0500

    📄 Search articles in artic#indx with turbo stream

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,19 @@ class ArticlesController < ApplicationController
 
   # GET /articles or /articles.json
   def index
-    @articles = Article.all
+    if params[:query].present?
+      @articles = Article.search(params[:query])
+    else
+      @articles = Article.all
+    end
+
+    if turbo_frame_request?
+      render turbo_stream: turbo_stream.replace(
+        "articles",
+        partial: "articles",
+        locals: { articles: @articles }
+      )
+    end
   end
 
   # GET /articles/1 or /articles/1.json
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :content, presence: true
+
+  scope :search, -> (query) { where("lower(title) LIKE ?", "%#{query.downcase}%") }
 end
